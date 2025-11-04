bca4862d2c9bccd71b8e3e39118257a3c2501bab
Lucas Pavone <lucaspavone@icloud.com>
2025-11-04 16:48:54 +0100

    ca fonctionne pas le curl

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,22 @@
-const expressApp = require("express");
+const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const dotenvIndex = require("dotenv");
-dotenvIndex.config();
+const dotenv = require("dotenv");
+dotenv.config();
 const heroRoutes = require("./routes/heroRoutes");
 
-const app = expressApp();
+const app = express();
 app.use(cors());
-app.use(expressApp.json());
+app.use(express.json());
 
 mongoose
 	.connect(process.env.MONGO_URI)
 	.then(() => console.log("MongoDB connecté"))
 	.catch((err: any) => console.error(err));
+
+app.get("/", (_req: any, res: { send: (arg0: string) => void }) => {
+	res.send("Serveur backend OK !");
+});
 
 app.use("/api/heroes", heroRoutes);
 
--- backend/src/routes/heroRoutes.ts
+++ backend/src/routes/heroRoutes.ts
@@ -1,12 +1,15 @@
-const express = require("express");
 const heroController = require("../controllers/heroController");
-
-const router = express.Router();
+const router = require("express").Router();
 
 router.get("/", heroController.getHeroes);
 router.get("/:id", heroController.getHeroById);
 router.post("/", heroController.addHero);
 router.put("/:id", heroController.updateHero);
 router.delete("/:id", heroController.deleteHero);
+router.get(
+	"/test",
+	(_req: any, res: { json: (arg0: { message: string }) => any }) =>
+		res.json({ message: "API fonctionne !" })
+);
 
 module.exports = router;
